4dd21fc794d9348bfd01f0447699e4982cece963
Elliot Schot <elliot.schot@gmail.com>
2025-07-13 18:23:35 +1000

    fix: remove redundant array initialization in home.packages add: include 'gh' in environment.systemPackages

--- home/common/packages.nix
+++ home/common/packages.nix
@@ -1,4 +1,3 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-  ];
+  home.packages = with pkgs; [];
 }
--- modules/common/packages.nix
+++ modules/common/packages.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     git
     just
+    gh
     jq
     direnv
     home-manager
