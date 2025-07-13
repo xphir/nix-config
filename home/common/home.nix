0f618e1b198548b66bf8424bd74f19bccad7dd66
Elliot Schot <Elliot.Schot@gmail.com>
2025-07-13 14:49:23 +1000

    fix version

--- home/common/home.nix
+++ home/common/home.nix
@@ -17,7 +17,7 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "24.11";
+    stateVersion = "25.05";
 
     # Common environment variables
     sessionVariables = {
